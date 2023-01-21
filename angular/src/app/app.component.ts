11b48d085a033d3fd2f151ff048b8ba1fb92a2d0
abhisheksaini123 <abhisaini54552@gmail.com>
2023-01-21 18:31:36 +0530

    now

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+
+})
+export class AppComponent {
+  title = 'angular-14';
+  subtitle = 'Open Source Knowledge Academe';
+
+//   getUser() {
+//     let User = "Guest";
+//     if (localStorage.getItem("login")) {
+//     }
+//     return User;
+//   }
+ }
